4a3e9034d5d3e274cc9e164f9962e884236a4ce1
Aleksandr Bondarev <a.bondarev@zmeke.com>
2019-10-02 10:11:10 +0300

    init

---
+++ src/main/java/ru/apbondarev/http2/Server.java
@@ -0,0 +1,100 @@
+package ru.apbondarev.http2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
+import io.vertx.ext.web.Router;
+
+public class Server extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        runExample(Server.class);
+    }
+
+    @Override
+    public void start() throws Exception {
+        Image image = new Image(vertx, "coin.png");
+
+        Router router = Router.router(vertx);
+
+        router.get("/").handler(ctx -> {
+            ctx.response()
+                    .putHeader("Content-Type", "text/html")
+                    .end(image.generateHTML(16));
+        });
+
+        router.get("/img/:x/:y").handler(ctx -> {
+            ctx.response()
+                    .putHeader("Content-Type", "image/png")
+                    .end(image.getPixel(Integer.parseInt(ctx.pathParam("x")), Integer.parseInt(ctx.pathParam("y"))));
+        });
+
+        vertx.createHttpServer(
+                new HttpServerOptions()
+                        .setSsl(true)
+                        .setUseAlpn(true)
+                        .setPemKeyCertOptions(new PemKeyCertOptions().setKeyPath("tls/server-key.pem").setCertPath("tls/server-cert.pem"))).requestHandler(router)
+                .listen(8443);
+    }
+
+    private static void runExample(Class<?> clazz) {
+        runExample("http2server/", clazz, new VertxOptions().setClustered(false), null);
+    }
+
+    private static void runExample(String exampleDir, Class<?> clazz, VertxOptions options, DeploymentOptions deploymentOptions) {
+        runExample(exampleDir + clazz.getPackage().getName().replace(".", "/"), clazz.getName(), options, deploymentOptions);
+    }
+
+    private static void runExample(String exampleDir, String verticleID, VertxOptions options, DeploymentOptions deploymentOptions) {
+        if (options == null) {
+            // Default parameter
+            options = new VertxOptions();
+        }
+        // Smart cwd detection
+
+        // Based on the current directory (.) and the desired directory (exampleDir), we try to compute the vertx.cwd
+        // directory:
+        try {
+            // We need to use the canonical file. Without the file name is .
+            File current = new File(".").getCanonicalFile();
+            if (exampleDir.startsWith(current.getName()) && !exampleDir.equals(current.getName())) {
+                exampleDir = exampleDir.substring(current.getName().length() + 1);
+            }
+        } catch (IOException e) {
+            // Ignore it.
+        }
+
+        System.setProperty("vertx.cwd", exampleDir);
+        Consumer<Vertx> runner = vertx -> {
+            try {
+                if (deploymentOptions != null) {
+                    vertx.deployVerticle(verticleID, deploymentOptions);
+                } else {
+                    vertx.deployVerticle(verticleID);
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        };
+        if (options.isClustered()) {
+            Vertx.clusteredVertx(options, res -> {
+                if (res.succeeded()) {
+                    Vertx vertx = res.result();
+                    runner.accept(vertx);
+                } else {
+                    res.cause().printStackTrace();
+                }
+            });
+        } else {
+            Vertx vertx = Vertx.vertx(options);
+            runner.accept(vertx);
+        }
+    }
+}
